b66000d68a0f631c33a7b64832e83c487e1cd95b
elahe01~ <elahe.narimani@gmail.com>
2023-09-01 16:44:01 +0330

    add sum2

---
+++ script.js
@@ -0,0 +1,9 @@
+let numberOfRepetition=5;
+let str='wow!';
+let string="";
+let newString;
+    for(let i=0; i<numberOfRepetition ; i++){
+      string="o"+string;    
+    }
+    newString="w"+`${string}`+"w"+"!";
+    console.log(newString)
